0e077b60f45ac7c1753d58ed277dbd40a2a39542
shoshana lagasa <shoshanalagasa850@gmail.com>
2021-12-13 20:56:22 -0600

    TESTSPA

--- store/Links.js
+++ store/Links.js
@@ -1,3 +1,6 @@
-export default ["Home", "Bio", "Gallery", "Register"];
-
-
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Gallery", text: "Gallery" },
+  { title: "Bio", text: "Bio" },
+  { title: "Register", text: "Register" }
+];
