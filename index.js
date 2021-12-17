04582d6a123e6df78b9321ab64bfe3c9a1d91f77
shoshana lagasa <shoshanalagasa850@gmail.com>
2021-12-16 20:43:31 -0600

    TESTSPA

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
+import axios from "axios";
+
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 
-const router = new Navigo("/");
+import dotenv from "dotenv";
+dotenv.config();
 
-router
-  .on({
-    "/": () => render(state.Home),
-    ":page": params => {
-      let page = capitalize(params.page);
-      render(state[page]);
-    }
-  })
-  .resolve();
+const router = new Navigo("/");
 
 function render(st) {
   document.querySelector("#root").innerHTML = `
@@ -60,3 +55,48 @@ function addEventListeners(st) {
     });
   }
 }
+
+// pizza
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    }
+  }
+});
+
+// router
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
--- store/Home.js
+++ store/Home.js
@@ -1,4 +1,5 @@
 export default {
   header: "my Home",
-  view: "Home"
+  view: "Home",
+  weather: {}
 };
